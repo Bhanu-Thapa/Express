ab3f2e96226319abecd4e963b26c2e38c3608e11
BhanuThapa <bhanuthapa252@gmail.com>
2023-02-02 23:39:55 +0530

    Response Html and Json

--- nav.js
+++ nav.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my Home page');
+  res.write('<h1>html line </h1>');
+  res.send();
 });
 
 app.get('/about', (req, res) => {
@@ -11,11 +12,33 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
-  res.send('welcome to my Contact page');
+  res.status(200).send('welcome to my Contact page');
 });
 
 app.get('/temp', (req, res) => {
-  res.status(200).send('welcome to my Temp page');
+  res.send([
+    {
+      id: 1,
+      name: 'Taddy',
+    },
+    {
+      id: 1,
+      name: 'Taddy',
+    },
+  ]);
+});
+
+app.get('/json', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Bhanu',
+    },
+    {
+      id: 1,
+      name: 'Bhanu',
+    },
+  ]);
 });
 
 app.listen(port, () => {
