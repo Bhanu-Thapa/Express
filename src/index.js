6f4a078749d03b9f500142acdd7e1126d8305df6
BhanuThapa <bhanuthapa252@gmail.com>
2023-02-03 00:57:27 +0530

    serve html css and js files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// relative absolute - path
+// console.log(__dirname);
+
+// console.log(path.join(__dirname, '../public'));
+
+const staticPath = path.join(__dirname, '../public');
+
+// builtin middleware
+app.use(express.static(staticPath));
 
 app.get('/', (req, res) => {
   res.send('Hello World From The Express');
